99e18f6b934ef54acefd9d7371ebd107c19d0fb3
burdin.p <burdin.p@chibbis.ru>
2021-05-07 23:12:07 +0300

    Adding css, sass, postcss

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './styles/index.scss'
+
+const App = () => {
+    return (
+        <>
+        <div>
+            <h1 className="body">
+                Hi!
+            </h1>
+        </div>
+        <div>
+
+        </div>
+        </>
+    )
+}
+
+export default App;
